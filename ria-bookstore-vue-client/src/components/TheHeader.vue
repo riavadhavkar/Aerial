44f662df2662f73a27c7b8f7340ec2ae251b272c
Ria Vadhavkar <vadhavkar.ria@gmail.com>
2023-02-26 02:14:34 -0500

    initial commit for P3

---
+++ ria-bookstore-vue-client/src/components/TheHeader.vue
@@ -0,0 +1,57 @@
+<script setup lang="ts">
+import TheHeaderDropdown from "@/components/TheHeaderDropdown.vue";
+</script>
+
+<style scoped>
+header {
+  display: flex;
+  flex-direction: row;
+  padding: 1em;
+  background: var(--secondary-background-color);
+
+  gap: 1em;
+}
+
+.text-logo {
+  font-family: var(--title-font-family);
+}
+
+.search-bar {
+  padding: 0.5em;
+}
+
+@media (max-width: 600px) {
+  header {
+    flex-direction: column;
+  }
+}
+</style>
+
+<template>
+  <header class="container">
+    <section class="bookstore-logo">
+      <router-link to="/">
+        <img
+          src="/site-images/bookstore-logo.png"
+          alt="Another Bookstore Logo"
+          width="150"
+          height="auto"
+        />
+      </router-link>
+    </section>
+    <section class="title-and-search-bar">
+      <h1>
+        <router-link to="/" class="text-logo"> Another Bookstore </router-link>
+      </h1>
+      <form action="category.html">
+        <input type="text" class="search-bar" /><br />
+        <input type="submit" class="button search-button" value="Search" />
+      </form>
+    </section>
+    <section class="header-dropdown-and-cart">
+      <button class="button">cart (0)</button>
+      <button class="button">login</button>
+      <the-header-dropdown></the-header-dropdown>
+    </section>
+  </header>
+</template>
---
+++ ria-bookstore-vue-client/src/views/HomeView.vue
@@ -0,0 +1,37 @@
+<script setup lang="ts">
+import TheHomeCategoryList from "@/components/TheHomeCategoryList.vue";
+</script>
+
+<style scoped>
+.home-page {
+  display: flex;
+}
+
+.welcome-text {
+  width: 20em;
+}
+</style>
+<template>
+  <div class="home-page">
+    <section class="welcome-text flow-content container dark-background">
+      <h2>Welcome to Another Bookstore</h2>
+      <p>
+        Another Bookstore is a client-side application written using Vue
+        components.
+      </p>
+      <p>
+        It includes a home page and a category page. Both pages have the same
+        header and footer.
+      </p>
+      <p>
+        You can navigate to the category page by choosing any category from the
+        dropdown menu in the header. You can navigate to the home page by
+        clicking on the logo in the header.
+      </p>
+      <p>Take some time and explore the code.</p>
+    </section>
+    <section class="category-images container">
+      <the-home-category-list></the-home-category-list>
+    </section>
+  </div>
+</template>
